300da1cbc8acb90c1f3e1c252488208dfde5a3eb
Josh Wulf <josh.wulf@camunda.com>
2025-11-11 02:20:17 +1300

    feat: update for 8.8.0

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is a demo of the [Camunda 8 SDK for Node.js](https://www.npmjs.com/package/@camunda8/sdk).
 
-It requires either a Camunda 8 Platform SaaS account, which you can get for free [here](https://signup.camunda.com/accounts), or running the Self-Managed stack via docker-compose using the configuration [here]([https://github.com/camunda/camunda-platform](https://github.com/camunda/camunda-8-js-sdk/blob/main/docker/docker-compose-multitenancy.yml)).
+This demonstrates the use of the Orchestration Cluster (REST) API, available from Camunda 8.8 onward.
 
 ## Setup
 
@@ -12,49 +12,49 @@ It requires either a Camunda 8 Platform SaaS account, which you can get for free
 npm i
 ```
 
-- Create a cluster in Camunda 8 Platform SaaS
-- Create an API client in the Web Console (instructions [here](https://docs.camunda.io/docs/next/guides/setup-client-connection-credentials/))
-- Put the client credentials environment variables in a file `.env` in the root of the project
-- Run the application with `npm start`
+- Download [Camunda Run](https://docs.camunda.io/docs/self-managed/quickstart/developer-quickstart/c8run/) to run Camunda 8 locally; or
+- Run `docker-compose -f docker/docker-compose.yml up -d`; or
+- Create a cluster in [Camunda SaaS](https://camunda.io).
+    - Create an API client in the Web Console (instructions [here](https://docs.camunda.io/docs/next/guides/setup-client-connection-credentials/))
 
-## Operation
+## Environment Configuration
 
-The application will deploy a BPMN process model to the cluster, then start an instance. A task worker services the first task. These operations use the Zeebe API.
+Camunda Run: 
 
-The next task in the process model is a human task. The Tasklist API is used to claim, then complete the task. This is accomplished by a "human task worker" that polls every three seconds.
+```bash
+export ZEEBE_REST_ADDRESS='http://localhost:8088/v2'
+```
 
-Finally, the Operate API is used to retrieve the XML of the process model.
+Docker:
 
-## Credentials
+```bash
+export ZEEBE_REST_ADDRESS='http:///localhost:8080/v2'
+```
 
 Camunda SaaS:
 
 ```bash
-export ZEEBE_ADDRESS='5c34c0a7-...-125615f7a9b9.syd-1.zeebe.camunda.io:443'
+export ZEEBE_REST_ADDRESS='https://5c34c0a7-...-125615f7a9b9.syd-1.zeebe.camunda.io'
 export ZEEBE_CLIENT_ID='yvvURO9TmBnP3...'
 export ZEEBE_CLIENT_SECRET='iJJu-SHgUt...'
-export CAMUNDA_TASKLIST_BASE_URL='https://syd-1.tasklist.camunda.io/5c34c0a7-...-125615f7a9b9'
-export CAMUNDA_OPTIMIZE_BASE_URL='https://syd-1.optimize.camunda.io/5c34c0a7-...-125615f7a9b9'
-export CAMUNDA_OPERATE_BASE_URL='https://syd-1.operate.camunda.io/5c34c0a7-...-125615f7a9b9'
 export CAMUNDA_OAUTH_URL='https://login.cloud.camunda.io/oauth/token'
-export CAMUNDA_MODELER_BASE_URL='https://modeler.cloud.camunda.io/api'
 ```
-Self-hosted:
+Self-hosted with OAuth:
 
 ```bash
 # Self-Managed
-export ZEEBE_ADDRESS='localhost:26500'
+export ZEEBE_REST_ADDRESS='http://localhost:8088/v2'
 export ZEEBE_CLIENT_ID='zeebe'
 export ZEEBE_CLIENT_SECRET='zecret'
 export CAMUNDA_OAUTH_URL='http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token'
-export CAMUNDA_TASKLIST_BASE_URL='http://localhost:8082'
-export CAMUNDA_OPERATE_BASE_URL='http://localhost:8081'
-export CAMUNDA_OPTIMIZE_BASE_URL='http://localhost:8083'
-export CAMUNDA_MODELER_BASE_URL='http://localhost:8070/api'
-
-# Needed for Multi-Tenancy
-export CAMUNDA_TENANT_ID='<default>'
-
-# TLS for gRPC is on by default. If the Zeebe broker is not secured by TLS, turn it off
-export CAMUNDA_SECURE_CONNECTION=false
 ```
+
+## Operation
+
+The application will deploy a BPMN process model to the cluster, then start an instance. A task worker services the first task. 
+
+The next task in the process model is a human task. The program polls for new user tasks to claim, then completes the user task. This is accomplished by a "human task worker" that polls every second.
+
+## Run
+
+- Run the application with `npm start`
--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,111 @@
-import {Camunda8} from '@camunda8/sdk'
+import { Camunda8, OrchestrationLifters } from '@camunda8/sdk'
 
-import chalk from 'chalk'
-import * as path from 'path'
-import { config } from 'dotenv' 
-config()
+const camunda = new Camunda8().getOrchestrationClusterApiClient()
 
-const c8 = new Camunda8()
-const zbc = c8.getZeebeGrpcApiClient()
-const operate = c8.getOperateApiClient()
-const optimize = c8.getOptimizeApiClient() // unused
-const tasklist = c8.getTasklistApiClient()
-
-const getLogger = (prefix: string, color: any) => (msg: string) => console.log(color(`[${prefix}] ${msg}`))
-
-async function main() {
-    const log = getLogger('Zeebe', chalk.greenBright) 
-    zbc 
-    const res = await zbc.deployResource({
-        processFilename: path.join(process.cwd(), 'resources', 'c8-sdk-demo.bpmn') 
-    })
-    log(`Deployed process ${res.deployments[0].process.bpmnProcessId}`)
-
-    const p = await zbc.createProcessInstanceWithResult({
-        bpmnProcessId: `c8-sdk-demo`, 
-        variables:{
-            humanTaskStatus: 'Needs doing'
-        }
-    })
-
-    log(`Finished Process Instance ${p.processInstanceKey}`)
-    log(`humanTaskStatus is "${p.variables.humanTaskStatus}"`)
-    log(`serviceTaskOutcome is ${p.variables.serviceTaskOutcome}`)
-
-    await new Promise(res => setTimeout(() => res(null), 5000))
-    const historicalProcessInstance = await operate.getProcessInstance(p.processInstanceKey).catch(e => {
-        if (e.response.body.status === 404) {
-            log(chalk.redBright(`[Operate] ${e.response.body.message}`))
-            return null
-        }
-    })
-    console.log(historicalProcessInstance)
-    if (historicalProcessInstance?.state == 'COMPLETED') {
-        const variables = await operate.getJSONVariablesforProcess(historicalProcessInstance.key)
-        log(chalk.redBright('\n[Operate] Variables:', JSON.stringify(variables, null, 2)))
-    }
-
-    const bpmn = await operate.getProcessDefinitionXML(p.processDefinitionKey)
-
-    log(chalk.redBright('\n[Operate] BPMN XML:', bpmn))
-}
-
-console.log(`Creating worker...`)
-zbc.createWorker({
-    taskType: 'service-task',
-    taskHandler: job => {
-        const log = getLogger('Zeebe Worker', chalk.blueBright)
-        log(`handling job of type ${job.type}`)
+// Job worker
+const worker = camunda.createJobWorker({
+    jobType: 'service-task',
+    workerName: 'test-worker',
+    maxParallelJobs: 20,
+    pollIntervalMs: 1000,
+    pollTimeoutMs: 50_000,
+    jobTimeoutMs: 5000,
+    jobHandler: (job) => {
+        console.log(`[worker]: Completing job ${job.jobKey} from process ${job.processInstanceKey}\n`)
         return job.complete({
-            serviceTaskOutcome: 'We did it!'
+            someNumberField: -1,
         })
-    }
+    },
 })
 
-console.log(`Starting human task poller...`)
-setInterval(async () => {
-    const log = getLogger('Tasklist', chalk.yellowBright)
-    const res = await tasklist.searchTasks({
-        state: 'CREATED'
+async function main() {
+    const res = await camunda.deployResourcesFromFiles([
+        './resources/c8-sdk-demo.bpmn',
+    ]);
+    const { processDefinitionKey } = res.processes[0];
+    const { processInstanceKey } = await camunda.createProcessInstance({
+        processDefinitionKey,
+        variables: {
+            userTaskCompleted: false,
+        },
+    });
+    console.log(`\nStarted process ${processInstanceKey}\n`)
+
+    // User task poller
+    const last = new Set<OrchestrationLifters.UserTaskKey>();
+    const userTaskPoller = camunda.searchUserTasks(
+        {
+            filter: {
+                state: 'CREATED',
+            },
+        },
+        {
+            // To set up a subscription, set waitUpToMs to Infinity
+            consistency: {
+                waitUpToMs: Infinity,
+                pollIntervalMs: 1_000,
+                // predicate now becomes a polling subscription function
+                predicate: async (results) => {
+                    // polling memoization - handles idempotency with eventually consistent mutation
+                    const current = results.items.filter((item) => !last.has(item.userTaskKey));
+                    last.clear();
+                    results.items.forEach((task) => last.add(task.userTaskKey));
+                    for (const userTask of current) {
+                        console.log(
+                            `[usertask poller]: Claiming task ${userTask.userTaskKey} from process ${userTask.processInstanceKey}\n`
+                        );
+                        await camunda.assignUserTask({
+                            userTaskKey: userTask.userTaskKey,
+                            assignee: 'jwulf',
+                        });
+
+                        console.log(
+                            `[usertask poller]: Completing user task ${userTask.userTaskKey} from process ${userTask.processInstanceKey}\n`
+                        );
+                        await camunda.completeUserTask({
+                            userTaskKey: userTask.userTaskKey,
+                            variables: {
+                                userTaskCompleted: true,
+                            },
+                        });
+                    }
+                    return false; // return false to keep polling
+                },
+            },
+        }
+    )
+    userTaskPoller.catch(e => {
+        if (e.name === 'CancelSdkError') {
+            return // swallow cancelation
+        }
     })
-    if (res.length > 0) {
-        log(`fetched ${res.length} human tasks`)
-        res.forEach(async task => {
-            log(`claiming task ${task.id} from process ${task.processInstanceKey}`)
-            const t = await tasklist.assignTask({
-                taskId: task.id, 
-                assignee: 'demobot',
-                allowOverrideAssignment: true
-            })
-            log(`servicing human task ${t.id} from process ${t.processInstanceKey}`)
-            await tasklist.completeTask(t.id, {
-                humanTaskStatus: 'Got done'
-            })
-        })
-    } else {
-        log('No human tasks found')
-    }
-}, 3000)
 
-
+    // Wait for our process to be completed
+    await camunda.searchProcessInstances(
+        {
+            filter: {
+                processInstanceKey,
+                state: 'COMPLETED',
+            },
+        },
+        { consistency: { waitUpToMs: 10_000 } }
+    );
+    console.log('Process completed, retrieving variables...\n');
+    const variables = await camunda.searchVariables(
+        {
+            filter: {
+                processInstanceKey,
+            },
+        },
+        { consistency: { waitUpToMs: 5_000 } }
+    );
+    const finalValues = variables.items
+        .map(item => ({ [item.name]: JSON.parse(item.value) }))
+        .reduce((curr, prev) => ({ ...curr, ...prev }), {})
+    console.log(`Process instance ${processInstanceKey} completed with variables:`, JSON.stringify(finalValues, null, 2));
+    console.log(`\n`)
+    userTaskPoller.cancel()
+    worker.stop()
+}
 
 main()
